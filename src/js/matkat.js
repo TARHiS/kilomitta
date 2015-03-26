d876b039b556c5d8751949ef0b7e6f0acb4a5958
Harry Karvonen <harry.karvonen@gmail.com>
2015-03-26 14:07:42 +0200

    Matkat: Välimatkojen näyttäminen luotu.

--- src/js/matkat.js
+++ src/js/matkat.js
@@ -1,7 +1,8 @@
 // vi: et sw=2 fileencoding=utf8
 //
 
-var matkat = storage.get("matkat").reverse();
+var matkat = storage.get("matkat") || [];
+matkat = matkat.reverse();
 
 function initMatkat() {
   $.each(matkat, function(index, matka) {
@@ -27,6 +28,12 @@ function initMatkat() {
         ).append(
           $("<button/>", {
             "type": "button",
+            "class": "btn btn-default matka-open pull-right glyphicon glyphicon-folder-open",
+            "data-matka": index,
+          })
+        ).append(
+          $("<button/>", {
+            "type": "button",
             "class": "btn btn-default matka-edit pull-right glyphicon glyphicon-edit",
             "data-matka": index,
           })
@@ -36,8 +43,50 @@ function initMatkat() {
   });
 }
 
+function appendValimatka(index, valimatka) {
+  $("#open-dialog-valimatkat tbody").append(
+    $("<tr/>", {"data-index": index}).append(
+      $("<td/>").text(
+        index + 1
+      )
+    ).append(
+      $("<td/>").html(
+        (valimatka.selite || "")
+      )
+    ).append(
+      $("<td/>").text(
+        moment(valimatka.lopetusaika).format("DD.MM.YYYY HH:mm")
+      )
+    ).append(
+      $("<td/>").html(
+        valimatka.kmlkm
+      )
+    ).append(
+      $("<button/>", {
+        "class": "btn btn-link glyphicon glyphicon-edit"
+      })
+    )
+  );
+}
+
 $("#btn-takaisin").click(function(){
   document.location = "index.html";
+});
+
+$("#matkat tbody").on("click", "button.matka-open", function (e) {
+  var btn = $(e.target);
+  var matka = matkat[btn.data("matka")];
+
+  $("#open-dialog")
+  $("#open-dialog-selite").text(matka.selite || "");
+  $("#open-dialog-tarkoitus").text(matka.tarkoitus || "");
+  $("#open-dialog-alkulukema").text(matka.alkulukema || "");
+  $("#open-dialog-loppulukema").text(matka.loppulukema || "");
+
+  $("#open-dialog-valimatkat tbody").html("");
+  $.each(matka.valimatkat, appendValimatka);
+
+  $("#open-dialog").modal("show");
 });
 
 $("#matkat tbody").on("click", "button.matka-edit", function (e) {
@@ -45,18 +94,10 @@ $("#matkat tbody").on("click", "button.matka-edit", function (e) {
   var matka = matkat[btn.data("matka")];
 
   $("#edit-dialog-save").data("matka", btn.data("matka"));
-  if (matka.selite) {
-    $("#edit-dialog-selite").val(matka.selite);
-  }
-  if (matka.tarkoitus) {
-    $("#edit-dialog-tarkoitus").val(matka.tarkoitus);
-  }
-  if (matka.alkulukema) {
-    $("#edit-dialog-alkulukema").val(matka.alkulukema);
-  }
-  if (matka.loppulukema) {
-    $("#edit-dialog-loppulukema").val(matka.loppulukema);
-  }
+  $("#edit-dialog-selite").val(matka.selite || "");
+  $("#edit-dialog-tarkoitus").val(matka.tarkoitus || "");
+  $("#edit-dialog-alkulukema").val(matka.alkulukema || "");
+  $("#edit-dialog-loppulukema").val(matka.loppulukema || "");
   $("#edit-dialog").modal("show");
 });
 
